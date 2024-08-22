a0fa0000a3ecb72baa86d82f2a1f4ad6d56a4ba8
oli-huttmacher <olihuttmacher@gmail.com>
2024-08-22 15:49:08 +0200

    add div>nav>li *3

--- src/index.js
+++ src/index.js
@@ -2,8 +2,38 @@ console.log(`hello webpack`);
 import './styles.css';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const element = document.createElement('div');
-  element.className = 'bg-blue-500 text-white p-4';
-  element.textContent = 'Hello, Tailwind CSS with Webpack!';
-  document.body.appendChild(element);
+  /*div*/
+  const divEl = document.createElement('div');
+  
+  
+  /*nav */
+  const navEl = document.createElement('nav');
+  navEl.className = 'flex justify-around inline-block bg-slate-500 p-5 list-none ';
+  
+  
+  /*li */
+  const liEl1 = document.createElement('li');
+  liEl1.textContent = 'Acceuil';
+  liEl1.className = 'text-white '
+
+  const liEl2 = document.createElement('li');
+  liEl2.textContent = 'Game';
+  liEl2.className = 'text-white'
+
+  const liEl3 = document.createElement('li');
+  liEl3.textContent = 'Contact';
+  liEl3.className = 'text-white'
+  
+  /*div > ul */
+  divEl.append(navEl);
+  /*nav > li */
+  navEl.append(liEl1,liEl2,liEl3);
+
+
+  /*render DOM elements */
+  document.body.appendChild(divEl);
+
+
+
+
 });
